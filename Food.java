e454c78b56f5074fd4c1ed24c919c49227c03b98
Aryan <axb3230@mavs.uta.edu>
2026-04-07 02:16:02 -0500

    Enhance Food class with selection and calorie calculation
    Added methods to manage food selection and calculate total calories.

--- Food.java
+++ Food.java
@@ -8,9 +8,14 @@ Display its calories
 
 */
 
+import java.util.ArrayList;
+
+
+
 public class Food {
 String foodName;
 int calories;
+    private static ArrayList<Food> selectedFoods = new ArrayList<>();
 
 public Food(String foodName , int calories){
     this.foodName = foodName;
@@ -22,6 +27,35 @@ public String getFoodName(){
 public int getCalories(){
     return calories;
 }
+public static ArrayList<Food> getfFoods(){
+    ArrayList<Food> foods = new ArrayList<>();
+    foods.add(new Food("Eggs" , 150));
+    foods.add(new Food("Daal" , 150));
+    foods.add(new Food("Meat" , 150));
+    foods.add(new Food("Rice" , 150));
+    foods.add(new Food("Chicken" , 150));
+return foods;
 
+
+}
+public void showFoodList(ArrayList<Food> foods){
+    System.out.println("-------Avaialbale foods are:");
+    for(Food f: foods){
+        System.out.println(f.getFoodName() +  " -" + f.getCalories() +"- "+ "cal/100g");
+    }
+
+
+}
+    public void selectFood(Food f) {
+        selectedFoods.add(f);
+        System.out.println(f.getFoodName() + " added!");
+    }
+
+    // NEW: return total calories of everything selected
+    public int getTotalCalories() {
+        int total = 0;
+        for (Food f : selectedFoods) total += f.getCalories();
+        return total;
+    }
 }
 
