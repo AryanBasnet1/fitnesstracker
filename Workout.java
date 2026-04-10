34f0b33ff35d860023051a2857535feab531e5dc
Aryan <axb3230@mavs.uta.edu>
2026-04-10 14:57:54 -0500

    Update Workout.java

--- Workout.java
+++ Workout.java
@@ -1,21 +1,52 @@
+import java.util.ArrayList;
+
 public class Workout {
-    String name;
-    int calories;
-    String effort;
-    public Workout(String name , int calories , String effort){
-this.name= name;
-this.calories=calories;
-this.effort = effort;
-    }
-    public String getName(){
-        return name;
+    String workoutName;
+    int caloriesBurned;
+    private static ArrayList<Workout> completedWorkouts = new ArrayList<>();
 
+    public Workout(String workoutName, int caloriesBurned) {
+        this.workoutName = workoutName;
+        this.caloriesBurned = caloriesBurned;
     }
-    public int getCalories(){
-        return calories;
 
+    public String getWorkoutName() {
+        return workoutName;
     }
-    public String getEffort(){
-        return effort;
+
+    public int getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public static ArrayList<Workout> getWorkouts() {
+        ArrayList<Workout> workouts = new ArrayList<>();
+        workouts.add(new Workout("Running", 300));
+        workouts.add(new Workout("Cycling", 250));
+        workouts.add(new Workout("Swimming", 400));
+        workouts.add(new Workout("Weightlifting", 200));
+        workouts.add(new Workout("Yoga", 150));
+        return workouts;
+    }
+
+    public void showWorkoutList(ArrayList<Workout> workouts) {
+        System.out.println("------- Available Workouts are:");
+        for (Workout w : workouts) {
+            System.out.println(w.getWorkoutName() + " - " + w.getCaloriesBurned() + " cal burned");
+        }
+    }
+
+    public void selectWorkout(Workout w) {
+        completedWorkouts.add(w);
+        System.out.println(w.getWorkoutName() + " added!");
+    }
+
+    public int getTotalCaloriesBurned() {
+        int total = 0;
+        for (Workout w : completedWorkouts) total += w.getCaloriesBurned();
+        return total;
+    }
+
+    public static ArrayList<Workout> getCompletedWorkouts() {
+        return completedWorkouts;
     }
 }
